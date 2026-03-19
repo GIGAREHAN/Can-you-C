51813cd9f70b7c4476b77670d8f71bb34b6297fd
Rehan Adatiya <105909757+GIGAREHAN@users.noreply.github.com>
2026-03-19 21:39:00 +0530

    Add variable declaration and initialization example

---
+++ Variable-Declaration.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int number = 10;        // variable declaration + initialization
+    float price = 25.75;
+    
+    printf("Number: %d\n", number);
+    printf("Price: %.2f\n", price);
+
+    return 0;
+}
