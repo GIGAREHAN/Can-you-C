9affb3c4348eb6d3ec937c0973e03fe1dc33f146
Rehan Adatiya <105909757+GIGAREHAN@users.noreply.github.com>
2026-03-19 21:37:28 +0530

    Add Escape-Sequence.c with various print examples

---
+++ Escape-Sequence.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Line1\nLine2\n");
+    printf("Tab\tSpace\n");
+    printf("Path: C:\\Program Files\\\n");
+    printf("Quote: \"Hello World\"\n");
+    return 0;
+}
